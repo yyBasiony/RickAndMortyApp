ea1cb33c90d0a3a051e20f5ab60772ed6d0df38b
yasmine ibrahim <iayasmine34@gmail.com>
2026-07-29 02:54:45 +0300

    add all features

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:rick_morty_app/app/app.dart';
 
-import 'package:rick_morty_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
